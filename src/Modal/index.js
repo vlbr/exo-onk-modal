2474bc79353c271df75d81d619bdf033ac24e1bd
jonathangiamp <jonathan.giamporcaro@gmail.com>
2020-05-20 15:21:01 +0200

    jonathan giamporcaro

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Button } from "@ornikar/kitt";
+import styles from "./Modal.module.css";
 
-export default function Modal() {
-  return <div>TODO</div>;
-}
+const Modal = ({ description, hasHeader, hasFooter }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        {hasHeader ? (
+          <header className={styles.header}>
+            <p className={styles.headerTitle}>Title</p>
+            <Button icon="icon" />
+          </header>
+        ) : null}
+        <div className={styles.body}>
+          <p className={styles.bodyContent}>{description}</p>
+        </div>
+        {hasFooter ? (
+          <footer className={styles.footer}>
+            <Button />
+            <Button />
+          </footer>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
