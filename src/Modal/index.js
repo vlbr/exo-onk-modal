acfdfca1ade6c6819894672d609553d061a9a20e
Timur Rustamov <timur.rustamov@epitech.eu>
2020-07-21 12:59:27 +0200

    WIP

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,5 +1,98 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-export default function Modal() {
-  return <div>TODO</div>;
-}
+import { Typography, Button } from "@ornikar/kitt";
+
+import "./Modal.css";
+
+const Portal = (props) => {
+  const { children } = props;
+
+  return ReactDOM.createPortal(children, document.body);
+};
+
+const Heading = (props) => {
+  return (
+    <div className="heading">
+      <Typography.h2 base="header4">Title</Typography.h2>
+      <Button icon="cross" />
+    </div>
+  );
+};
+
+const Separator = () => {
+  return <hr className="separator" />;
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <Typography.p base="header5">
+        Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator. Reference site about Lorem Ipsum,
+        giving information on its origins, as well as a random Lipsum
+        generator.Reference site about Lorem Ipsum, giving information on its
+        origins, as well as a random Lipsum generator.Reference site about Lorem
+        Ipsum, giving information on its origins, as well as a random Lipsum
+        generator.Reference site about Lorem Ipsum, giving information on its
+        origins, as well as a random Lipsum generator.Reference site about Lorem
+        Ipsum, giving information on its origins, as well as a random Lipsum
+        generator.Reference site about Lorem Ipsum, giving information on its
+        origins, as well as a random Lipsum generator.Reference site about Lorem
+        Ipsum, giving information on its origins, as well as a random Lipsum
+        generator.Reference site about Lorem Ipsum, giving information on its
+        origins, as well as a random Lipsum generator.Reference site about Lorem
+        Ipsum, giving information on its origins, as well as a random Lipsum
+        generator.Reference site about Lorem Ipsum, giving information on its
+        origins, as well as a random Lipsum generator.
+        Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.Reference site about Lorem Ipsum, giving information on its origins, as
+        well as a random Lipsum generator.
+      </Typography.p>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Button type="link">Link</Button>
+      <Button type="primary">Link</Button>
+    </div>
+  );
+};
+
+const Modal = () => {
+  return (
+    <Portal>
+      <div className="root">
+        <div className="paper">
+          <div className="heading-container">
+            <Heading />
+            <Separator />
+          </div>
+
+          <Body />
+
+          <div className="footer-container">
+            <Separator />
+
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </Portal>
+  );
+};
+
+export default Modal;
